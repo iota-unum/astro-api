cc3f452236be9b6007d6f1e0715b536901a3f978
luigi <luigilamastra@gmail.com>
2023-05-05 19:22:23 +0200

    fixed port

--- index.js
+++ index.js
@@ -77,4 +77,5 @@ await tweetAllImages(-1)
 
 
 
-app.listen(3000, ()=> console.log('listening'))
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
